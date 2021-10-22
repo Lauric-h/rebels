d1ac62c2f0f1d366da7d4f6108a12f44ca95f1cb
Luke <l.duboispagnon@gmail.com>
2021-10-22 13:19:09 +0200

    first rebels

---
+++ src/test/java/com/springmsa/rebels/RebelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmsa.rebels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
